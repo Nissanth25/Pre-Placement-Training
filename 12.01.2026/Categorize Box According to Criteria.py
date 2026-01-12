3d170410a98fbb2a18c4dd0cf49655eaa158825f
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2026-01-12 14:19:17 +0530

    Create Categorize Box According to Criteria.py

---
+++ 12.01.2026/Categorize Box According to Criteria.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def categorizeBox(self, length, width, height, mass):
+        volume = length * width * height
+        Bulky = False
+        Heavy = False
+
+        if any(x >= 10**4 for x in (length, width, height, mass)) or volume >= 10 ** 9:
+            Bulky = True
+
+        if mass >= 100:
+            Heavy = True
+        
+        if Bulky and Heavy:
+            return "Both"
+        elif not Bulky and not Heavy:
+            return "Neither"
+        elif Bulky and not Heavy:
+            return "Bulky"
+        elif Heavy and not Bulky:
+            return "Heavy"
