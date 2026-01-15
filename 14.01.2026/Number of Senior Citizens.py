cfaa2f5d6dc4ad7aa1bb0ec91cab01dc856a52c2
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2026-01-15 08:47:23 +0530

    Create Number of Senior Citizens.py

---
+++ 14.01.2026/Number of Senior Citizens.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countSeniors(self, details):
+        counter = 0
+        for i in details:
+            if int(i[11:13]) > 60:
+                counter+=1
+        return counter
+      
